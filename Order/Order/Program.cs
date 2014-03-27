91b3f6059d9041eac0d7d9689a18758b000cfe62
Bodik69 <bsmachylo86@gmail.com>
2014-03-27 16:05:35 +0200

    Add Order project

---
+++ Order/Order/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace PMI21_TeachingPractice
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml;
+    using System.Xml.Linq;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] buf = File.ReadAllLines("Data.txt");
+            int size = buf.Length;
+            List<Order> orders = new List<Order>();
+            for (int i = 0; i < buf.Length; i++)
+            {
+                buf[i] = buf[i].PadRight(buf[i].Length + 1, ' ');
+                Order temp = new Order();
+                temp.MakeOrder(buf[i]);
+                orders.Add(temp);
+            }
+
+            for (int i = 0; i < orders.Count; i++)
+            {
+                orders[i].Write();
+            }
+
+            XmlWriter fs = XmlWriter.Create("Result.xml");
+            fs.WriteStartElement("head");
+            fs.WriteFullEndElement();
+            fs.Close();
+            XmlDocument document = new XmlDocument();
+            document.Load("Result.xml");
+            XmlElement root = document.DocumentElement;
+            for (int i = 0; i < orders.Count; i++)
+            {
+                orders[i].Write(document);
+            }
+
+            document.Save("Result.xml");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Order/Order/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Order")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Order")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("8c6dc130-ff3c-4642-9fb8-c9190f35c8d5")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
